c48c1aa7454660fff5f7f4882260b017ed601c5c
childressdev <jcampbell@childressagency.com>
2017-07-18 10:50:00 -0400

    header fixed, started work on hero

--- header.php
+++ header.php
@@ -13,62 +13,57 @@
   <body <?php body_class(isset($class) ? $class : ''); ?>>
 
     <div id="fb-root"></div>
-      <script>(function(d, s, id) {
+    <script>
+      (function(d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
         js = d.createElement(s); js.id = id;
         js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.9&appId=724983870970769";
         fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));</script>
+      }(document, 'script', 'facebook-jssdk'));
+    </script>
 
-
-      <nav class="navbar navbar-default" role="navigation">
-        <div class="container">
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-            <a class="header-logo" href="<?php echo home_url(); ?>">
-              <img src="<?php echo get_template_directory_uri(); ?>/images/logo-new.png" alt=" Logo" class="logo-img">
-            </a>
-            <div class="header-contact-info">
-              <div class="phone">
-                <p>FREDERICKSBURG LOCATION</p>
-                <a href="tel:5407100303">540-710-0303</a>
-              </div>
-              <div class="hours">
-                <p>MON - WED 8A - 6P<br />THU - FRI 8A - 7P<br />SAT 8A - 5P</p>
-              </div>
-            </div><!-- flex -->
-          </div>
-          <!-- Collect the nav links, forms, and other content for toggling -->
-           <?php 
-            wp_nav_menu( array(
-              'menu' => 'Main', 
-              'menu_class' => 'nav navbar-nav navbar-right', 
-              'depth'=> 3, 
-              'container'=> 'div', 
-              'container_class' => 'navbar-collapse collapse',
-              'container_id' => 'navbar',
-              'walker'=> new Bootstrap_Walker_Nav_Menu)); ?>
+    <nav class="navbar navbar-default" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="header-logo" href="<?php echo home_url(); ?>">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/logo-new.png" alt=" Logo" class="logo-img">
+          </a>
+          <div class="header-contact-info">
+            <div class="phone">
+              <p>FREDERICKSBURG LOCATION</p>
+              <a href="tel:5407100303">540-710-0303</a>
+            </div>
+            <div class="hours">
+              <p>MON - WED 8A - 6P<br />THU - FRI 8A - 7P<br />SAT 8A - 5P</p>
+            </div>
+          </div><!-- flex -->
         </div>
-      </nav>
-
+        <!-- Collect the nav links, forms, and other content for toggling -->
+          <?php 
+          wp_nav_menu( array(
+            'menu' => 'Main', 
+            'menu_class' => 'nav navbar-nav navbar-right', 
+            'depth'=> 3, 
+            'container'=> 'div', 
+            'container_class' => 'navbar-collapse collapse',
+            'container_id' => 'navbar',
+            'walker'=> new Bootstrap_Walker_Nav_Menu)); ?>
+      </div>
       <div class="nav-line"></div>
+    </nav>
 
-      <?php if(is_front_page()): ?>
-
-
-    <section id="hp-hero" class="carousel slide" data-ride="carousel">
-
-      <div class="carousel-inner" role="listbox">
-        <?php
-        if( have_rows('home_slider') ):
-           $i=1; while ( have_rows('home_slider') ) : the_row(); ?>
+    <?php if(is_front_page()): ?>
+      <section id="hp-hero" class="carousel slide" data-ride="">
+        <div class="carousel-inner" role="listbox">
+          <?php if( have_rows('home_slider') ): $i=1; while ( have_rows('home_slider') ) : the_row(); ?>
             <div class="item <?php if ($i == 1) echo 'active'; ?> ">
-              <div id="mainPhoto" style="background-image: url(<?php  the_sub_field('slider_image'); ?>);"></div>
+              <div class="slide-background" style="background-image: url(<?php  the_sub_field('slider_image'); ?>);"></div>
               <div class="caption-wrapper">
                 <div class="caption">
                   <h1><?php the_sub_field('slider_title'); ?></h1>
@@ -76,30 +71,27 @@
                 </div><!-- caption -->
               </div><!-- caption-wrapper -->
             </div><!-- item -->
-            <?php $i++;  endwhile;
-            else :
-          endif;
-          ?>
-          <div class="container">
-            <div class="row">
-              <div class="sliderButtons ">
-                <div class="col-xs-6">
-                  <div class="left">
-                    <a href="<?php echo home_url(); ?>/our-work/" class="btn-main">OUR WORK</a>
-                  </div>
+          <?php $i++;  endwhile; endif; ?>
+        </div>
+        <div class="container">
+          <div class="row">
+            <div class="sliderButtons ">
+              <div class="col-xs-6">
+                <div class="left">
+                  <a href="<?php echo home_url(); ?>/our-work/" class="btn-main">OUR WORK</a>
                 </div>
-                <div class="col-xs-6">
-                  <div class="right">
-                    <a href="<?php echo home_url(); ?>/virtual-designer/" class="btn-main">VIRTUAL DESIGNER</a>
-                  </div>
+              </div>
+              <div class="col-xs-6">
+                <div class="right">
+                  <a href="<?php echo home_url(); ?>/virtual-designer/" class="btn-main">VIRTUAL DESIGNER</a>
                 </div>
-              </div><!-- sliderButtons -->
-              <a href="#finance" id="scrollDown">
-                <span class="glyphicon glyphicon-menu-down"></span>
-              </a>
-            </div><!-- row -->
-          </div><!-- contaner -->
-      </div><!-- carousel-inner -->
+              </div>
+            </div><!-- sliderButtons -->
+            <a href="#finance" id="scrollDown">
+              <span class="glyphicon glyphicon-menu-down"></span>
+            </a>
+          </div><!-- row -->
+        </div><!-- contaner -->
           <!-- Controls -->
         <a class="left carousel-control" href="#hp-hero" role="button" data-slide="prev">
           <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
@@ -109,19 +101,20 @@
           <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
           <span class="sr-only">Next</span>
         </a>
-    </section><!-- hp-hero -->
-        <?php else: ?>
-        <section id="heroSection">
-        <div id="mainPhoto" style="background-image: url(<?php the_field('hero_image'); ?>);">
+      </section><!-- hp-hero -->
+    <?php else: ?>
+      <section id="heroSection">
+        <div id="mainPhoto" style="background-image: url(<?php the_field('hero_image'); ?>);"></div>
         <?php if(get_field('hero_title')): ?>
           <h1><?php the_field('hero_title'); ?></h1>
         <?php else: ?>
           <h1><?php echo get_the_title(); ?></h1>
         <?php endif; ?>
+
         <?php if(get_field('hero_sub-title')): ?>
           <?php the_field('hero_sub-title'); ?>
         <?php else: ?>
           <?php echo get_the_sub-title(); ?>
         <?php endif; ?>
-      <?php endif; ?>
       </section><!-- heroSection -->
+    <?php endif; ?>
