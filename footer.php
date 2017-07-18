4744e729701bdc0584dcce6ea6213b0458b89890
childressdev <jcampbell@childressagency.com>
2017-07-18 17:48:21 -0400

    new logos and homepage

--- footer.php
+++ footer.php
@@ -2,27 +2,29 @@
   <div id="darkBar">
     <div class="container">
       <div class="row">
-        <div class="col-cs-12 col-md-3">
-          <a class="navbar-brand" href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/logo-new.png" alt=" Logo" class="logo-img"></a>
+        <div class="col-md-3">
+          <a class="footer-logo" href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/logo-new.png" alt=" Logo" class="logo-img"></a>
         </div>
 
-        <div class="col-cs-12 col-md-3 ">
+        <div class="col-sm-5 col-md-4">
           <div class="footerSocials">
-            <a href="<?php the_field('twitter_link', 'option'); ?>" target="_blank"><div class="twitter"></div></a>
-            <a href="<?php the_field('instagram_link', 'option'); ?>" target="_blank"><div class="instagram"></div></a>
-            <a href="<?php the_field('linkedin_link', 'option'); ?>" target="_blank"><div class="linkedIn"></div></a>
-            <a href="<?php the_field('facebook_link', 'option'); ?>" target="_blank"><div class="facebook"></div></a>
+            <a href="<?php the_field('twitter_link', 'option'); ?>" target="_blank" class="twitter"></a>
+            <a href="<?php the_field('instagram_link', 'option'); ?>" target="_blank" class="instagram"></a>
+            <a href="<?php the_field('linkedin_link', 'option'); ?>" target="_blank" class="linkedIn"></a>
+            <a href="<?php the_field('facebook_link', 'option'); ?>" target="_blank" class="facebook"></a>
           </div>
         </div>
-        <div class="col-cs-12 col-md-5 col-md-offset-1 footerInfo">
-          <div class="col-xs-6">
-            <h4>FREDERICKSBURG LOCATION</h4>
-            <a href="tel:54071003003">540-710-0303</a>
-          </div>
-          <div class="col-xs-6 footerHours">
-            <h3>MON - WED: 8A-6P</h3>
-            <h3>THUR - FRI: 8A-7P</h3>
-            <h3>SAT:8A-5P</h3>
+        <div class="col-sm-7 col-md-5 footerInfo">
+          <div class="row">
+            <div class="col-sm-7">
+              <h4>FREDERICKSBURG LOCATION</h4>
+              <a href="tel:54071003003">540-710-0303</a>
+            </div>
+            <div class="col-sm-5 footerHours">
+              <p>MON - WED: 8A-6P</p>
+              <p>THUR - FRI: 8A-7P</p>
+              <p>SAT:8A-5P</p>
+            </div>
           </div>
         </div>
       </div><!-- row -->
@@ -47,15 +49,15 @@
       <div class="col-xs-12 col-sm-6 footerRight">
         <h2>NAVIGATION</h2>
         <nav class="footer-nav">
-          <?php wp_nav_menu( array('menu' => 'Main', 'menu_class' => 'nav navbar-nav navbar-right', 'depth'=> 3, 'container'=> false, 'walker'=> new Bootstrap_Walker_Nav_Menu)); ?>
+          <?php wp_nav_menu( array('menu' => 'Main', 'menu_class' => '', 'depth'=> 3, 'container'=> false, 'walker'=> new Bootstrap_Walker_Nav_Menu)); ?>
         </nav>
       </div>
       <div class="col-xs-12 footerBottom">
         <p>VIRGINIA BETTER BUSINESS BUREAU MEMBER<P>
         <p>
-          &copy; <?php echo date('Y'); ?><?php bloginfo('name'); ?>ALL RIGHTS RESERVERED. -WEBSITE CREATED BY
+          &copy;<?php echo date('Y'); ?><?php bloginfo('name'); ?> ALL RIGHTS RESERVED. -WEBSITE CREATED BY
 
-          <a href="#" target="_blank">THE CHILDRESS AGENCY</a>
+          <a href="https://childressagency.com" target="_blank">THE CHILDRESS AGENCY</a>
         </p>
       </div>
   </div><!-- row -->
--- front-page.php
+++ front-page.php
@@ -1,20 +1,17 @@
 <?php get_header(); ?>
-<div id="main-container" >	<section id="finance">
-		<div class="container">
-			<div class="row">
-				<div class="col-xs-10 col-xs-offset-1">
-					<div id="zoom-box">
-						<div class="col-xs-5 leftFinance">
-							<h2>0%</h2>
-						</div>
-						<div class="col-xs-7 rightFinance">
-							<h3>FINANCING</h3>
-							<h4>FOR QUALIFIED BUYERS</h4>
-						</div>
-					</div>
+<div id="main-container" >	
+	<section id="finance">
+		<div class="media-group">
+			<div class="media-left media-middle">
+				<h2>0%</h2>
+			</div>
+			<div class="media-body media-middle">
+				<div class="media-header">
+					<h3>Financing</h3>
 				</div>
-			</div><!-- row -->
-		</div><!-- contaqiner -->
+				<h4>For Qualified Buyers</h4>
+			</div>
+		</div>
 	</section><!-- finance -->
 	<section id="aboutHomeSection">
 			<div class="container">
@@ -78,38 +75,32 @@
 	</section>
 	<section id="selections">
 		<div class="container">
-			<div class="row">
-				<div class="col-xs-6 col-xs-offset-4 col-sm-offset-6 selectionList">
+				<div class="selectionList">
 					<ul>
-						<li><a href="<?php echo home_url(); ?>">CERAMIC</a></li>
-						<li><a href="<?php echo home_url(); ?>">GLASS</a></li>
-						<li><a href="<?php echo home_url(); ?>">GRANITE</a></li>
-						<li><a href="<?php echo home_url(); ?>">METALS</a></li>
-						<li><a href="<?php echo home_url(); ?>">PORCELAIN</a></li>
-						<li><a href="<?php echo home_url(); ?>">TRAVERTINE</a></li>
-						<li><a href="<?php echo home_url(); ?>">TUMBLE</a></li>
+						<li>CERAMIC</li>
+						<li>GLASS</li>
+						<li>GRANITE</li>
+						<li>METALS</li>
+						<li>PORCELAIN</li>
+						<li>TRAVERTINE</li>
+						<li>TUMBLE</li>
 					</ul>
-					<a href="<?php echo home_url(); ?>/virtual-designer" class="btn-main">VIEW OUR SELECTION</a>
-					<a href="<?php echo home_url(); ?>/virtual-designer" class="btn-main">VIRTUAL DESIGNER</a>
+					<a href="<?php echo home_url(); ?>/virtual-designer" class="btn-main btn-tan">VIEW OUR SELECTION</a>
+					<a href="<?php echo home_url(); ?>/virtual-designer" class="btn-main btn-tan">VIRTUAL DESIGNER</a>
 				</div>
-			</div><!-- row -->
 		</div><!-- container -->
 	</section><!-- selections -->
 	<section id="brands">
 		<div class="container">
-			<div class="row">
-					<h2>BRANDS WE CARRY</h2>
-					<div class="col-xs-8 col-xs-offset-2 col-sm-12 col-sm-offset-0 grid">
-							<?php if( have_rows('brands') ):
-					    	while ( have_rows('brands') ) : the_row();?>
-									<div class="grid-item">
-					        	<img src="<?php the_sub_field('brand_icon'); ?>" alt="Icon" />
-									</div>
-					    	<?php endwhile; else :?>
-							</div>
-							<?php endif; ?>
-				</div><!-- row -->
-			</div><!-- container -->
+			<h2>BRANDS WE CARRY</h2>
+			<div class="brand-list">
+				<ul class="list-unstyled list-inline">
+					<?php if( have_rows('brands') ): while ( have_rows('brands') ) : the_row();?>
+						<li><img src="<?php the_sub_field('brand_icon'); ?>" alt="Icon" /></li>
+					<?php endwhile; endif;?>
+				</ul>
+			</div>
+		</div><!-- container -->
 	</section><!-- brands -->
 	<section id="reviews">
 		<div class="container">
